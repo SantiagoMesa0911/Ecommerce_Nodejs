972d734e8143fa27e96b14d07e547048c64f35b8
Santiago Mesa Parra <santiagomesa0911@gmail.com>
2022-05-23 15:43:06 -0500

    country

--- models/product.js
+++ models/product.js
@@ -5,6 +5,10 @@ const Productschema = new mongoose.Schema({
 		type: String,
 		required: [true, 'El nombre es requerido'],
 		minlength: [5, 'El nombre no debe ser menor a 5 caracteres']
+	},
+	country: {
+		type: String,
+		required: [true, 'country is required'],
 	}
 }, {
 	timestamps: true,
